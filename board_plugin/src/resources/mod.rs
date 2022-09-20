c1d13ebe5b8bff0b88e19a5cf741a7dc2ba2fd3c
Kuba Karpierz <admiral-akk@users.noreply.github.com>
2022-09-20 14:47:22 -0400

    Working through minesweeper tutorial

---
+++ board_plugin/src/resources/mod.rs
@@ -0,0 +1,5 @@
+pub(crate) mod board;
+pub mod board_options;
+pub(crate) mod bounds;
+pub(crate) mod tile;
+pub(crate) mod tile_map;
---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use bevy::prelude::*;
+
+#[cfg(feature = "debug")]
+use bevy_inspector_egui::WorldInspectorPlugin;
+use board_plugin::resources::board_options::BoardOptions;
+use board_plugin::BoardPlugin;
+fn main() {
+    let mut app = App::new();
+    // Window setup
+    app.insert_resource(WindowDescriptor {
+        title: "Mine Sweeper!".to_string(),
+        width: 700.,
+        height: 800.,
+        ..Default::default()
+    })
+    // Bevy default plugins
+    .add_plugins(DefaultPlugins);
+    #[cfg(feature = "debug")]
+    // Debug hierarchy inspector
+    app.add_plugin(WorldInspectorPlugin::new());
+
+    app.insert_resource(BoardOptions {
+        map_size: (20, 20),
+        bomb_count: 40,
+        tile_padding: 3.0,
+        ..Default::default()
+    });
+    // Startup system (cameras)
+    app.add_startup_system(camera_setup);
+    app.add_plugin(BoardPlugin);
+    // Run the app
+    app.run();
+}
+
+fn camera_setup(mut commands: Commands) {
+    // 2D orthographic camera
+    commands.spawn_bundle(Camera2dBundle::default());
+}
