8367e0e2eeef6a159e647724dbf95bd3e3cfa5be
Kuba Karpierz <admiral-akk@users.noreply.github.com>
2022-10-14 13:27:49 -0400

    removed old attack code

--- tower_defense_plugin/src/systems/attack.rs
+++ tower_defense_plugin/src/systems/attack.rs
@@ -1,38 +1,12 @@
-use bevy::prelude::{Component, Entity, EventReader, EventWriter, Parent, Query, Res, With};
+use bevy::prelude::{Component, EventReader, EventWriter, Parent, Query, Res};
 
 use crate::{
     components::{
-        allegiance::{Allegiance},
-        attacks::attack::Attack,
-        coordinates::Coordinates,
-        health::Health,
-        power::Power,
+        allegiance::Allegiance, attacks::attack::Attack, coordinates::Coordinates,
     },
-    events::{ActiveAction, ActiveUnit, AttackEvent},
+    events::{ActiveAction, AttackEvent},
     resources::board::Board,
 };
-
-pub fn attack(
-    board: Res<Board>,
-    attackers: Query<(&Coordinates, &Power, &Allegiance)>,
-    targets: Query<(Entity, &Allegiance), With<Health>>,
-    mut active_ert: EventReader<ActiveUnit>,
-    mut attack_ewr: EventWriter<AttackEvent>,
-) {
-    for active in active_ert.iter() {
-        let (coord, power, allegiance) = attackers.get(active.0).unwrap();
-        let potential_targets = board.neighbouring_entities(coord);
-        for target in potential_targets {
-            if let Ok((target, target_allegiance)) = targets.get(target) {
-                if !allegiance.eq(target_allegiance) {
-                    attack_ewr.send(AttackEvent(target, power.0 as i32));
-                    break;
-                }
-            }
-        }
-    }
-}
-
 pub fn try_attack<T: Attack + Component>(
     board: Res<Board>,
     attacks: Query<(&Parent, &T)>,
