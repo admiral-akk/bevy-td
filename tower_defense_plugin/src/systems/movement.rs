49d844c104739087701723a0f029d51c684302c1
Kuba Karpierz <admiral-akk@users.noreply.github.com>
2022-10-14 09:58:55 -0400

    removed old movement code

--- tower_defense_plugin/src/systems/movement.rs
+++ tower_defense_plugin/src/systems/movement.rs
@@ -1,9 +1,4 @@
-use std::collections::VecDeque;
-
-use bevy::{
-    prelude::{Component, EventReader, Query, Res},
-    utils::HashSet,
-};
+use bevy::prelude::{Component, EventReader, Query, Res};
 
 use crate::{
     components::{allegiance::Allegiance, coordinates::Coordinates, movement::Movement},
@@ -29,52 +24,6 @@ pub fn apply_move<T: Movement + Component>(
                         *coord = new_coord;
                     }
                 }
-            }
-        }
-    }
-}
-
-pub fn next(
-    start: &Coordinates,
-    targets: Vec<Coordinates>,
-    board: &Res<Board>,
-) -> Option<Coordinates> {
-    let mut visited: HashSet<Coordinates> = HashSet::from_iter(targets.iter().map(|c| c.clone()));
-    let mut queue = VecDeque::from_iter(targets.iter().map(|c| c.clone()));
-    while let Some(next) = queue.pop_front() {
-        for neighbour in next.orthogonal_neighbours() {
-            if neighbour.eq(start) {
-                return Some(next);
-            } else if board.entities.contains_key(&neighbour) {
-                continue;
-            } else if !visited.contains(&neighbour) {
-                visited.insert(neighbour);
-                queue.push_back(neighbour);
-            }
-        }
-    }
-    return None;
-}
-
-pub fn movement(
-    mut entities: Query<(&mut Coordinates, &Allegiance)>,
-    mut active_ert: EventReader<ActiveUnit>,
-    board: Res<Board>,
-) {
-    for active in active_ert.iter() {
-        let a = entities.get(active.0).unwrap().1;
-        let targets: Vec<Coordinates> = entities
-            .iter()
-            .filter(|(_, allegiance)| !a.eq(allegiance))
-            .map(|(coord, _)| *coord)
-            .collect();
-
-        let mut coord = entities.get_mut(active.0).unwrap().0;
-        bevy::log::error!("Trying move: {:?}", coord);
-        if let Some(next) = next(&coord, targets, &board) {
-            bevy::log::error!("Move to: {:?}", next);
-            if !board.entities.contains_key(&next) {
-                *coord = next;
             }
         }
     }
